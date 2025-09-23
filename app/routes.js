7a41f77fa93f9cee0fd746ac3e599e79075cadcb
Balvant <balvant@m2webdesigning.com>
2025-09-23 10:30:27 +0530

    22-09

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,8 @@
+// import { flatRoutes } from "@remix-run/fs-routes";
+
+// export default flatRoutes();
+// app/routes.js
 import { flatRoutes } from "@remix-run/fs-routes";
 
-export default flatRoutes();
+/** @type {import('@remix-run/route-config').RouteConfig} */
+export default flatRoutes("routes"); // 👈 routes folder nu naam aapવું જરુરી
