7ace7f31fd59124cd66310f5be7a8afaead1d813
Balvant <balvant@m2webdesigning.com>
2025-09-23 12:39:57 +0530

    22-09

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,6 @@
 // export default flatRoutes();
 // app/routes.js
 // app/routes.js
-import { flatRoutes } from "@remix-run/dev";
+import { flatRoutes } from "@remix-run/fs-routes";
 export default flatRoutes("routes");
 
