11c14d1df5c97c8e16f0eaa4752bd3eee1dcd96a
Balvant <balvant@m2webdesigning.com>
2025-09-23 14:26:20 +0530

    fix: remove programmatic routes refs (app/routes.ts) and use file-based routing

---
+++ app/routes.js
@@ -0,0 +1,8 @@
+// import { flatRoutes } from "@remix-run/fs-routes";
+
+// export default flatRoutes();
+// app/routes.js
+// app/routes.js
+import { flatRoutes } from "@remix-run/fs-routes";
+export default flatRoutes("routes");
+
