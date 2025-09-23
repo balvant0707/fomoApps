1df9010350230705cce31b7ae1710d702510ffcc
Balvant <balvant@m2webdesigning.com>
2025-09-23 16:23:48 +0530

    fix: provide valid Remix RouteConfig in root routes.js

---
+++ routes.js
@@ -0,0 +1,5 @@
+﻿/** @type {import('@remix-run/dev').RouteConfig} */
+export default [
+  { id: "root", file: "root.jsx" },      // જો તમે app/root.tsx વાપરો છો તો "root.tsx" લખો
+  { path: "app", file: "routes/app.jsx" } // જો app/routes/app.tsx હોય તો "app.tsx" લખો
+];
