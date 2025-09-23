312df6160b60564c532293f038064e0de4eb6518
Balvant <balvant@m2webdesigning.com>
2025-09-23 14:18:59 +0530

    fix: remove programmatic routes file causing build error

--- app/routes.js
+++
@@ -1,8 +0,0 @@
-// import { flatRoutes } from "@remix-run/fs-routes";
-
-// export default flatRoutes();
-// app/routes.js
-// app/routes.js
-import { flatRoutes } from "@remix-run/fs-routes";
-export default flatRoutes("routes");
-
