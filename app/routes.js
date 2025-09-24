4f869f37143dfeba9cd9b6c2f38a5f2791f9dbaf
Balvant <balvant@m2webdesigning.com>
2025-09-24 09:17:26 +0530

    22-09

--- app/routes.js
+++ app/routes.js
@@ -1,2 +1,12 @@
 import { flatRoutes } from "@remix-run/fs-routes";
+
 export default flatRoutes();
+export const handle = {
+  id: "root",
+};
+export const unstable_shouldReload = () => false;
+export const unstable_isRouteErrorResponse = () => false;
+export const handleRouteError = () => null;
+export const handleRouteChange = () => null;
+export const handleRouteFocus = () => null;
+export const handleRouteBlur = () => null;
