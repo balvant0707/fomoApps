adfe988b9ff4b31668345755b4f6a9a935edc83b
Balvant <balvant@m2webdesigning.com>
2025-09-24 10:28:53 +0530

    22-09

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,28 @@
 ﻿// Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
 // stop passing in HOST, so we can remove this workaround after the next major release.
+// if (
+//   process.env.HOST &&
+//   (!process.env.SHOPIFY_APP_URL ||
+//     process.env.SHOPIFY_APP_URL === process.env.HOST)
+// ) {
+//   process.env.SHOPIFY_APP_URL = process.env.HOST;
+//   delete process.env.HOST;
+// }
+
+// /** @type {import('@remix-run/dev').AppConfig} */
+// module.exports = {
+//   ignoredRouteFiles: ["**/.*"],
+//   appDirectory: "app",
+//   serverModuleFormat: "cjs",
+//   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
+//   future: {},
+// };
+
+
+// remix.config.js (ESM)
+
+// HOST workaround same રાખો
 if (
   process.env.HOST &&
   (!process.env.SHOPIFY_APP_URL ||
@@ -10,11 +32,14 @@ if (
   delete process.env.HOST;
 }
 
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+import { defineConfig } from "@remix-run/dev";
+
+export default defineConfig({
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
-  serverModuleFormat: "cjs",
+
+  serverModuleFormat: "esm",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
+
   future: {},
-};
+});
